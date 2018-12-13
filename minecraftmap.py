14c2459879aa09d9a7a7d86e5d2d88ce8be61259
[Joshua Buseck] <[jbuseck697@lsr7.net]>
2018-12-13 13:24:09 -0600

    Added new file that allows to see each indiv. pixel as well as finishing last file that shows what block your standing on

--- minecraftmap.py
+++ minecraftmap.py
@@ -6,9 +6,36 @@ from time import sleep
 sense = SenseHat()
 mc = Minecraft.create()
 
+#blocks
+grass = 2
+
+water = 9
+
+sand = 12
+
+#colors
+green = (0, 255, 0)
+
+blue = (0, 0, 255)
+
+yellow = (255, 255, 0)
+
+#block clors
+colors = {
+    grass: green,
+    water: blue,
+    sand: yellow,
+}
+
+
 while True:
     x, y, z = mc.player.getTilePos()
     block = mc.getBlock(x, y-1, z)
-    print(block)
+    
+    if block in colors:
+        color = colors[block]
+        sense.clear(color)
+    else:
+        print("Dont't know block ID %s" % block)
     sleep(0.1)
     
---
+++ minecraftmap2.py
@@ -0,0 +1,10 @@
+from sense_hat import SenseHat
+
+from time import sleep
+
+
+sense = SenseHat()
+
+sense.clear()
+sense.set_pixel(0, 0, 255, 255, 255)
+
